cb5aab6b43d0c059bc81c06482fc00ef1bdb304d
LydiaSorbo <43254564+LydiaSorbo@users.noreply.github.com>
2023-04-05 17:22:13 -0700

    Update extra-1.sql

--- extra-1.sql
+++ extra-1.sql
@@ -1,1 +1,4 @@
+select title, first_name, last_name, age, city, state from persons where city = 'Phoenix' and title = 'Rev' and gender = 'Male';
+select title, first_name, last_name, age, city, state from persons where city = 'West Palm Beach' and title = 'Dr';
 
+--No, their ages aren't the same
